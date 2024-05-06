7405a54df826dceba9f19dce410ca39301cad153
arthur3r <arthurfreireribeiro19@gmail.com>
2024-05-06 00:12:58 -0300

    fix: prettier, eslint and errors

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,25 @@
-import Image from "next/image";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import Image from 'next/image'
+import { Button } from './ui/button'
+import { MenuIcon } from 'lucide-react'
 
 const Header = () => {
-  return ( 
-    <div className="flex justify-between pt-6 px-5">
-      <Image src="./logo-fsw-foods.svg" alt="FSW Foods" width={100} height={30} />
-      <Button size="icon" variant="outline" className="bg-transparent border-none">
+  return (
+    <div className="flex justify-between px-5 pt-6">
+      <Image
+        src="./logo-fsw-foods.svg"
+        alt="FSW Foods"
+        width={100}
+        height={30}
+      />
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
         <MenuIcon />
       </Button>
     </div>
-  );
+  )
 }
- 
-export default Header;
+
+export default Header
