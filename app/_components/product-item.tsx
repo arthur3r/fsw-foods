867c33d6b3f5dbb3f6ea4a36f1d616bd965b217c
arthur3r <arthurfreireribeiro19@gmail.com>
2024-05-07 13:37:54 -0300

    refactor: ensure product list recives products as prop

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -30,7 +30,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
           <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-white">
             <ArrowDownIcon size={18} />
             <span className="text-xs font-semibold">
-              {product.discountPercentage}
+              {product.discountPercentage}%
             </span>
           </div>
         )}
